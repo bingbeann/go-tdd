a5932032501521944e8be707b583040287844fc6
bingbeann <ruizhi@outlook.my>
2025-09-28 21:10:46 +0800

    refactor(hello): use map instead of switch
    Switch and map behaves similarly, except with switch changes had to be
    made on the switch itself + creating new constants, whereas map when
    updated will just work, this better support ETC (Easier To Change).

--- hello/hello.go
+++ hello/hello.go
@@ -6,33 +6,22 @@ func main() {
 	fmt.Println(Hello("world", ""))
 }
 
-const (
-	englishHelloPrefix = "Hello, "
-	spanishHelloPrefix = "Hola, "
-	frenchHelloPrefix  = "Bonjour, "
-
-	languageSpanish = "Spanish"
-	languageFrench  = "French"
-)
+var greetingPrefix = map[string]string{
+	"English": "Hello",
+	"Spanish": "Hola",
+	"French":  "Bonjour",
+}
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
 	}
 
-	prefix := getGreetingPrefix(language)
-
-	return prefix + name
-}
-
-func getGreetingPrefix(language string) (prefix string) {
-	switch language {
-	case languageSpanish:
-		prefix = spanishHelloPrefix
-	case languageFrench:
-		prefix = frenchHelloPrefix
-	default:
-		prefix = englishHelloPrefix
+	if language == "" {
+		language = "English"
 	}
-	return
+
+	prefix := greetingPrefix[language]
+
+	return fmt.Sprintf("%s, %s", prefix, name)
 }
