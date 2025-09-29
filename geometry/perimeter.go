8209c413f1d6718b015c38744cb4ec01a5b77a99
bingbeann <ruizhi@outlook.my>
2025-09-29 22:33:56 +0800

    feat(geometry): area

--- geometry/perimeter.go
+++ geometry/perimeter.go
@@ -3,3 +3,7 @@ package geometry
 func Perimeter(w, h float64) float64 {
 	return (w + h) * 2
 }
+
+func Area(w, h float64) float64 {
+	return w * h
+}
--- geometry/perimeter_test.go
+++ geometry/perimeter_test.go
@@ -5,6 +5,17 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 	got := Perimeter(10.0, 10.0)
 	want := 40.0
+	assertFloat(t, want, got)
+}
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 6.0)
+	want := 72.0
+	assertFloat(t, want, got)
+}
+
+func assertFloat(t testing.TB, want, got float64) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("want %.2f but got %.2f", want, got)
