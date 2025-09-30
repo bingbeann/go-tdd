51fb0ca3912652243bb2f79f656cf45008810716
bingbeann <ruizhi@outlook.my>
2025-10-01 01:00:56 +0800

    refactor(wallet): convey domain with int as Bitcoin

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -6,12 +6,12 @@ import (
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("want %d but got %d", want, got)
+		t.Errorf("want %s but got %s", want, got)
 	}
 }
