9e219cbb12bfafa4c9ff87d7de249a74e8e6f6ab
bingbeann <ruizhi@outlook.my>
2025-10-02 01:34:06 +0800

    test(countdown): assert calls order

--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -2,35 +2,72 @@ package countdown
 
 import (
 	"bytes"
+	"slices"
 	"testing"
 	"time"
 )
 
-type SpySleeper struct {
-	Calls int
+const (
+	write = "write"
+	sleep = "sleep"
+)
+
+type StubSleeper struct{}
+
+func (s *StubSleeper) Sleep(d time.Duration) {}
+
+type SpyCountdownOperations struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep(d time.Duration) {
-	s.Calls++
+func (s *SpyCountdownOperations) Sleep(d time.Duration) {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return len(p), nil
 }
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("print 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		stubSleeper := &StubSleeper{}
 
-	err := Countdown(buffer, 3, spySleeper)
-	if err != nil {
-		t.Fatalf("failed to countdown: %v", err)
-	}
+		err := Countdown(buffer, 3, stubSleeper)
+		if err != nil {
+			t.Fatalf("failed to countdown: %v", err)
+		}
 
-	got := buffer.String()
-	want := "3\n2\n1\nGo!"
+		got := buffer.String()
+		want := "3\n2\n1\nGo!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, wanted 3 but got %d", spySleeper.Calls)
-	}
+	t.Run("sleep before every print", func(t *testing.T) {
+		spyCountdownOperations := &SpyCountdownOperations{}
+
+		err := Countdown(spyCountdownOperations, 3, spyCountdownOperations)
+		if err != nil {
+			t.Fatalf("failed to countdown: %v", err)
+		}
+
+		got := spyCountdownOperations.Calls
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
