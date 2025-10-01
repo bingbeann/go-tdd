f627c818f3f6876fa9d8787f4d7da76992700064
bingbeann <ruizhi@outlook.my>
2025-10-02 01:23:27 +0800

    fix: main default sleeper

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/bingbeann/go-tdd/countdown"
 )
 
 func main() {
-	if err := countdown.Countdown(os.Stdout); err != nil {
+	if err := countdown.Countdown(os.Stdout, 3, &DefaultSleeper{}); err != nil {
 		log.Fatalf("failed to countdown: %v", err)
 	}
+}
+
+type DefaultSleeper struct{}
+
+func (s *DefaultSleeper) Sleep(d time.Duration) {
+	time.Sleep(d)
 }
