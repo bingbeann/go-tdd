f97f939a0c49e3f4af73d33158129702e9294e52
bingbeann <ruizhi@outlook.my>
2025-10-02 01:21:29 +0800

    feat(countdown): sleep between countdown

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -3,13 +3,20 @@ package countdown
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
-func Countdown(w io.Writer, n int) error {
+type Sleeper interface {
+	Sleep(time.Duration)
+}
+
+func Countdown(w io.Writer, n int, sleeper Sleeper) error {
 	for i := n; i >= 1; i-- {
 		if _, err := fmt.Fprintln(w, i); err != nil {
 			return err
 		}
+
+		sleeper.Sleep(1 * time.Second)
 	}
 
 	if _, err := fmt.Fprint(w, "Go!"); err != nil {
--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -3,12 +3,22 @@ package countdown
 import (
 	"bytes"
 	"testing"
+	"time"
 )
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep(d time.Duration) {
+	s.Calls++
+}
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	err := Countdown(buffer, 3)
+	err := Countdown(buffer, 3, spySleeper)
 	if err != nil {
 		t.Fatalf("failed to countdown: %v", err)
 	}
@@ -18,5 +28,9 @@ func TestCountdown(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, wanted 3 but got %d", spySleeper.Calls)
 	}
 }
