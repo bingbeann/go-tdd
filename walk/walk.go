c3b496c75cb891d69fbc90fcaafb3175c98438bc
bingbeann <ruizhi@outlook.my>
2025-10-14 23:56:44 +0800

    docs(walk): explain why not DRY

--- walk/walk.go
+++ walk/walk.go
@@ -5,6 +5,10 @@ import "reflect"
 func walk(x any, fn func(string)) {
 	val := getValue(x)
 
+	// On slice and struct, the looping logic is conceptually the same. The tutorial refactored to be more DRY.
+	// However, the current code provides more clarity. It is clear when you study how to handle a slice, that it
+	// iterates over each item and call walk recursively, instead of having to see values assigned to variables, and
+	// only know their purpose at the end, your eyes need to go up and down.
 	switch val.Kind() {
 	case reflect.Slice:
 		for i := range val.Len() {
