53224bfae980df99853c8120a610c360e1dcc20e
bingbeann <ruizhi@outlook.my>
2025-11-09 03:33:13 +0800

    feat(server): proper request cancellation

--- server/server_test.go
+++ server/server_test.go
@@ -1,23 +1,32 @@
 package server
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type SpyStore struct {
-	response string
+	response  string
+	cancelled bool
 }
 
 func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
 
 func TestServer(t *testing.T) {
 	t.Run("OK", func(t *testing.T) {
 		want := "hello, world"
-		svr := Server(&SpyStore{want, false})
+		store := &SpyStore{want, false}
+		svr := Server(store)
 
 		req := httptest.NewRequest(http.MethodGet, "/", nil)
 		resp := httptest.NewRecorder()
@@ -27,6 +36,29 @@ func TestServer(t *testing.T) {
 		got := resp.Body.String()
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
+		}
+
+		if store.cancelled {
+			t.Error("store should not have cancelled")
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		want := "hello, world"
+		store := &SpyStore{want, false}
+		svr := Server(store)
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingCtx, cancel := context.WithCancel(req.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		req = req.WithContext(cancellingCtx)
+
+		resp := httptest.NewRecorder()
+
+		svr.ServeHTTP(resp, req)
+
+		if !store.cancelled {
+			t.Error("store was not told to cancel")
 		}
 	})
 }
